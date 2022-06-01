cc013b6884378e7f370d3e56702c32f2187b70d4
jimish1806 <jimish957@gmail.com>
2022-06-02 02:16:23 +0530

    set avatar selection functionality

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 console.log("Jay Shree Ram");
 
+// ----------------> Grab forward and back Buttons & Sections <--------------- //
 let moveToAvatar = document.getElementById("moveToAvatar");
 let avatarPage = document.getElementById("avatarPage");
 let signUpArea = document.getElementById("signUpArea");
 let backArrow = document.getElementById("backArrow");
 
-
+// ------------------> Grab Input Field Details <----------------  //
 let userName = document.getElementById("name");
 let dob = document.getElementById("dob");
 let email = document.getElementById("email");
 let password = document.getElementById("password");
 
+// ------------> Grab Selected Avatar Image and Name Section <-------- //
 let avatarName = document.getElementById("avatarName");
+let selectedAvatar = document.getElementById("selectedAvatar");
 
+
+// ----------> EL oncliking submitting signUp form <------------ //
 moveToAvatar.addEventListener("click",(e)=>
 {
     if(avatarPage.classList.contains("classAvatarOut") || signUpArea.classList.contains("classSignUpPageIn"))
@@ -31,6 +36,7 @@ moveToAvatar.addEventListener("click",(e)=>
     e.preventDefault();
 })
 
+// -----------> EL on clicking back button of Avatar section Area <---------- //
 backArrow.addEventListener("click",()=>
 {
     if(avatarPage.classList.contains("classAvatarIn") || signUpArea.classList.contains("classSignUpPageOut"))
@@ -44,3 +50,16 @@ backArrow.addEventListener("click",()=>
     avatarPage.classList.add(`classAvatarOut`);
     signUpArea.classList.add(`classSignUpPageIn`);
 })
+
+// -----------------> Function to show selected Avatar <------------ //
+function avatarSelection(number)
+{
+    if(number < 5)
+    {
+        selectedAvatar.src = `images/avatar/female${number}.png`;
+    }
+    if(number > 4)
+    {
+        selectedAvatar.src = `images/avatar/male${number-4}.png`;
+    }
+}
