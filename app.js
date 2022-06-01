e1d8ae8103aef7e955a6b5b6b01468e1b8e1884e
jimish1806 <jimish957@gmail.com>
2022-06-02 01:26:46 +0530

    set animation for signUp page and avatar selection page

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 console.log("Jay Shree Ram");
+
+let moveToAvatar = document.getElementById("moveToAvatar");
+let avatarPage = document.getElementById("avatarPage");
+let signUpArea = document.getElementById("signUpArea");
+let backArrow = document.getElementById("backArrow");
+
+moveToAvatar.addEventListener("click",(e)=>
+{
+    if(avatarPage.classList.contains("classAvatarOut") || signUpArea.classList.contains("classSignUpPageIn"))
+    {
+        avatarPage.classList.remove(`classAvatarOut`);        
+
+        signUpArea.classList.remove(`classSignUpPageIn`);
+        
+    }
+
+    avatarPage.classList.add(`classAvatarIn`);
+    signUpArea.classList.add(`classSignUpPageOut`);
+    e.preventDefault();
+})
+
+backArrow.addEventListener("click",()=>
+{
+    if(avatarPage.classList.contains("classAvatarIn") || signUpArea.classList.contains("classSignUpPageOut"))
+    {
+        avatarPage.classList.remove(`classAvatarIn`);        
+
+        signUpArea.classList.remove(`classSignUpPageOut`);
+        
+    }
+
+    avatarPage.classList.add(`classAvatarOut`);
+    signUpArea.classList.add(`classSignUpPageIn`);
+})
